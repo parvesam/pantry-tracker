dce845284eb240ff37bd3c00a1bb78690bd69ba8
parvesam <parvesam@sheridancollege.ca>
2024-08-06 16:23:29 -0400

    add items, remove items and added the models

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import {useState, useEffect} from "react";
 import {firestore} from "@/firebase";
 import {Box, Typography} from "@mui/material";
 import {query, collection, getDocs} from "firebase/firestore";
+import e from "express";
 
 export default function Home() {
   const [inventory, setInventory] = useState([]);
@@ -21,12 +22,45 @@ export default function Home() {
       })
     })
     setInventory(inventoryList);
-    console.log(inventoryList);
   };
+
+  const removeItem = async (item) =>{
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()){
+      const {quantity} = docSnap.data();
+      if (quantity == 1){
+        await deleteDoc(docRef);
+      }
+      else{
+        await setDoc(docRef, {quantity: quantity - 1});
+      }
+    }
+    await updateInventory();
+  }
+
+  const addItem = async (item) =>{
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()){
+      const {quantity} = docSnap.data();
+      await setDoc(docRef, {quantity: quantity + 1});
+      }
+      else{
+        await setDoc(docRef, {quantity: 1});
+      }
+    }
+    await updateInventory();
+  }
 
   useEffect(()=>{
     updateInventory();
   }, []);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
 
   return (
@@ -34,13 +68,14 @@ export default function Home() {
     <Typography variant = "h1">Inventory Management</Typography>
     {
       inventory.forEach((item)=>{
-        console.log(item)
-        return(<Box>
+        console.log(item);
+        return(
+        <Box>
         {item.name}
         {item.count}
-        </Box>)
-      })
-    }
+        </Box>
+        )
+      })}
   </Box>
   )
-};
+}
