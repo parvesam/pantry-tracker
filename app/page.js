74d17ccd6b281a9b23690b25524226d5dbc85654
parvesam <parvesam@sheridancollege.ca>
2024-08-06 21:47:18 -0400

    completed the added, removed, updated plus added a search bar to easily find items...

--- app/page.js
+++ app/page.js
@@ -3,12 +3,13 @@ import Image from "next/image";
 import {useState, useEffect} from "react";
 import {firestore} from "@/firebase";
 import {Box, Modal, Typography, Stack, TextField, Button} from "@mui/material";
-import {query, collection, getDocs, deleteDoc, doc} from "firebase/firestore";
+import {query, collection, getDocs, deleteDoc, doc, getDoc, setDoc} from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setInventory] = useState([]);
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const [itemName, setItemName] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const updateInventory = async () =>{
     const snapshot = query(collection(firestore,"inventory"));
@@ -59,9 +60,15 @@ export default function Home() {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const filteredInventory = inventory.filter((item) =>
+  item.name.toUpperCase().includes(searchTerm.toUpperCase())
+
+);
+
+
 
   return (
-  <Box width = {"100vw"} height = {"100vh"} display= {"flex"} justifyContent={"center"} alignItems={"center"} gap={2}>
+  <Box width = {"100vw"} height = {"100vh"} display= {"flex"} flexDirection="column" justifyContent={"center"} alignItems={"center"} gap={2}>
     <Modal open={open} onClose={handleClose}>
       <Box position={"absolute"} top={"50%"} left={"50%"} width={400} bgcolor={"white"} border={"2px solid #000"} boxShadow={24} p={4} display={"flex"} flexDirection={"column"} gap={3} sx={{transform: "translate(-50%,-50%)",}}>
         <Typography variant = "h6">Add Item</Typography>
@@ -72,12 +79,57 @@ export default function Home() {
             onClick={()=>{
               addItem(itemName) 
               setItemName('')
-              handleClose()}}>
+              handleClose()}}> ADD
               </Button>
         </Stack>
       </Box>
     </Modal>
-    <Typography variant = "h1">Inventory Management</Typography>
+    <Button variant="contained" onClick={()=>{
+      handleOpen()
+    }}>
+      Add New Item
+      </Button>
+      <TextField
+        variant="outlined"
+        width={"800px"}
+        height={"100px"}
+        placeholder="Search items..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      <Box border={"1px solid #333"}>
+        <Box 
+        width={"800px"}
+        height={"100px"}
+        bgcolor={"#ADD8E6"}
+        display={"flex"}
+        alignItems="center"
+        justifyContent={"center"}>
+          <Typography variant="h2" color="#333">Inventory Items</Typography>
+        </Box>
+      <Stack width={"800px"} height={"300px"} spacing={2} overflow={"auto"}>
+        {
+          filteredInventory.map(({name, quantity})=>(
+            <Box key="name" width={"100%"} minHeight={"150px"} display="flex" alignItems={"center"} justifyContent={"space-between"} bgcolor={"f0f0f0"} padding={5}>
+              <Typography variant="h3" color="#333" textAlign="center">
+                {name.charAt(0).toUpperCase()+name.slice(1)}
+              </Typography> 
+              <Typography variant="h3" color="#333" textAlign="center">
+                {quantity}
+              </Typography>
+              <Stack direction="row" spacing={2}>
+              <Button variant="contained" onClick={()=>{
+                addItem(name)
+              }}>Add Item</Button>
+              <Button variant="contained" onClick={()=>{
+                removeItem(name)
+              }}>Remove Item</Button>
+              </Stack>
+            </Box>
+          ))}
+      </Stack>
+      </Box>
   </Box>
   )
 }
